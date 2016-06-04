17cec341294f23f8e687c495e8b908bf6970d58b
Kasper Baagø Jensen <kasper@edentic.dk>
2016-06-04 16:24:31 +0200

    Working method for getting artists

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
+var shuffle = require('shuffle-array');
 
 var apiEndPoint = 'http://10.10.5.123:8529/';
 var pictureDb = 'https://image.tmdb.org/t/p/w185';
@@ -10,11 +11,30 @@ var db = 'http://10.10.5.123:8529/_db/_system/off2016/fmk';
 app.use('/', express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+var getArtist = function(callback) {
+    request.get(apiEndPoint + '_db/_system/off2016/cast', function(err, httpResponse, body) {
+        var artist = JSON.parse(body);
+        var data = artist.filter(function(artist) {
+            if(artist.image_path) {
+                artist.image = pictureDb + artist.image_path;
+                return artist;
+            }
+        });
+
+        console.log(data);
+        data = shuffle.pick(data, {'picks': 3});
+        callback(data);
+    })
+};
+
 app.use('/api/startgame', function(req, res) {
     request.post(db, { }, function(err, httpResponse, body) {
         var object = JSON.parse(body);
-        res.json({
-            sid: object._id
+        getArtist(function(artists) {
+            res.json({
+                sid: object._id,
+                artists: artists
+            });
         });
     })
 });
