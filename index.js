39b62393fbb5a8242847035313da475d4cf37c7e
Kasper Baagø Jensen <kasper@edentic.dk>
2016-06-04 16:08:33 +0200

    Startgame method created

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+
+var apiEndPoint = 'http://10.10.5.123:8529/';
+var pictureDb = 'https://image.tmdb.org/t/p/w185';
+var db = 'http://10.10.5.123:8529/_db/_system/off2016/fmk';
 
 app.use('/', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api/startgame', function(req, res) {
+    request.post(db, { }, function(err, httpResponse, body) {
+        var object = JSON.parse(body);
+        res.json({
+            sid: object._id
+        });
+    })
+});
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
