8a102ec444514ebac4bd50295ef281bdcd56892e
Håkon Krogh <hakonhk@gmail.com>
2016-06-04 17:05:20 +0200

    Sessionid

--- js/main.js
+++ js/main.js
@@ -3,6 +3,26 @@ const Vue = require('vue');
 
 Vue.use(require('vue-resource'));
 
+//Vue.http.options.root = '/root';
+//Vue.http.headers.common['Authorization'] = 'Basic YXBpOnBhc3N3b3Jk';
+
+/*Vue.http.interceptors.push({
+    request: function (request) {
+
+    	// Set domain
+    	if (!request.url.indexOf('/api') !== 0) {
+    		request.url = `/api/${request.url}`.replace('//', '/');
+    	}
+
+        return request;
+    },
+
+    response: function (response) {
+        return response;
+    }
+});
+*/
+
 // The initial state
 const data = {
     sessionId: null,
@@ -14,10 +34,10 @@ const vm = new Vue({
     components: {
         app
     },
-    data,
     ready: function () {
-        console.log(this.$http);
-    }
+    	console.log("App ready...");
+    },
+    data
 });
 
 // Leak the vue instance
