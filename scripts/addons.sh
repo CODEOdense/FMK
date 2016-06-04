2080d2daff22acef3311b1943a3f42c9c1dda505
Jesper Blad Jensen <jesperbjensen@gmail.com>
2016-06-04 20:35:54 +0200

    Vagrant work

--- scripts/addons.sh
+++ scripts/addons.sh
@@ -17,9 +17,12 @@ wget https://www.arangodb.com/repositories/arangodb2/xUbuntu_14.04/Release.key
 sudo apt-key add Release.key
 sudo apt-add-repository 'deb https://www.arangodb.com/repositories/arangodb2/xUbuntu_14.04/ /'
 sudo apt-get update
+sleep 1
 sudo apt-get install arangodb=2.8.9 -y
 sudo sed -i "s/endpoint = tcp:\/\/127.0.0.1:8529/endpoint = tcp:\/\/192.168.57.10:8529/g" /etc/arangodb/arangod.conf
 sudo sed -i "s/endpoint = tcp:\/\/127.0.0.1:8529/endpoint = tcp:\/\/192.168.57.10:8529/g" /etc/arangodb/arangosh.conf
-sudo service arangodb restart
+sudo service arangodb stop
+sudo arangod --upgrade
+sudo service arangodb start
 sleep 2
 curl -H 'Content-Type: application/json' -X PUT -d '{"url": "CODEOdense/OFF16Data", "version": ""}' http://192.168.57.10:8529/_db/_system/_admin/aardvark/foxxes/git?mount=%2Foff2016
