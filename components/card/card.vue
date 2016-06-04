3d3359537c4544a651311067cbcc05e63c0e304f
Frederik Stahlfest Møller <frederik@edentic.dk>
2016-06-04 17:47:47 +0200

    Added fmk buttons to cards

--- components/card/card.vue
+++ components/card/card.vue
@@ -6,12 +6,22 @@
         <div class="card__name">
             <p>{{ text }}</p>
         </div>
+        <div class="fmk-buttons">
+            <btn text="Fuck" type="f"></btn>
+            <btn text="Marry" type="m"></btn>
+            <btn text="Kill" type="k"></btn>
+        </div>
     </div>
 </template>
 
 <script>
-    import './_card.scss'
+    import './_card.scss';
+
+    import btn from '../button/button.vue';
     export default {
+        components: {
+            btn
+        },
         props: ['text'],
         data: {
             text: ''
